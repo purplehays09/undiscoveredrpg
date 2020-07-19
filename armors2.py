e4d6ce1ba40eb46a2bf0a3178d4217a06dc44165
purplehays09 <purplehays527@gmail.com>
2020-07-19 13:23:04 -0600

    adding a second version of armors, all that is left is to do the features

---
+++ armors2.py
@@ -0,0 +1,62 @@
+armors_dict = {
+    "armor_name":{
+        "Name":"String",
+        "Type":"String_Light_Medium_Heavy",
+        "Durability":"Int",
+        "Features":[{"Dictionary":"Value"}]
+    },
+    "Unarmored":{
+        "Name":"Unarmored",
+        "Type":"Light",
+        "Durability":"0",
+        "Features":[{"Dictionary":"Value"}]
+    },
+    "Leather Armor":{
+        "Name":"Leather Armor",
+        "Type":"Light",
+        "Durability":"3",
+        "Features":[{"Dictionary":"Value"}]
+    },
+    "Gambeson":{
+        "Name":"Gambeson",
+        "Type":"Light",
+        "Durability":"3",
+        "Features":[{"Dictionary":"Value"}]
+    },
+    "Chain Mail":{
+        "Name":"Chain Mail",
+        "Type":"Medium",
+        "Durability":"4",
+        "Features":[{"Dictionary":"Value"}]
+    },
+    "Splint Mail":{
+        "Name":"Splint Mail",
+        "Type":"Medium",
+        "Durability":"4",
+        "Features":[{"Dictionary":"Value"}]
+    },
+    "Breast Plate":{
+        "Name":"Breast Plate",
+        "Type":"Medium",
+        "Durability":"4",
+        "Features":[{"Dictionary":"Value"}]
+    },
+    "Scale Mail":{
+        "Name":"Scale Mail",
+        "Type":"Heavy",
+        "Durability":"5",
+        "Features":[{"Dictionary":"Value"}]
+    },
+    "Plate Mail":{
+        "Name":"Plate Mail",
+        "Type":"Heavy",
+        "Durability":"5",
+        "Features":[{"Dictionary":"Value"}]
+    },
+    "Full Plate":{
+        "Name":"Full Plate",
+        "Type":"Heavy",
+        "Durability":"7",
+        "Features":[{"Dictionary":"Value"}]
+    },
+}
