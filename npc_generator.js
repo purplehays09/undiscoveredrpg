bd641232a567076a0d99357f4b07d13dc4dd527d
purplehays09 <purplehays527@gmail.com>
2020-07-24 18:35:18 -0600

    added an npc generator

---
+++ npc_generator.js
@@ -0,0 +1,711 @@
+const nameString = `Ruben
+Grooch
+Padarian
+Kammon
+Delvin
+Sheila
+Sonora
+Melvin
+Slythe
+Selma
+Rainthold
+Rory
+Melathar
+Rooster
+Edward
+Shayla
+Hartar
+Randal
+Michka
+Perchance
+Rikard
+Marelow
+Shint
+Tootsie
+Snartle
+Pat
+Rod
+Kiptka
+Ramona
+Lothar
+Abrielle
+Adair
+Adara
+Adriel
+Aiyana
+Alissa
+Alixandra
+Altair
+Amara
+Anatola
+Anya
+Arcadia
+Ariadne
+Arianwen
+Aurelia
+Aurelian
+Aurelius
+Avalon
+Acalia
+Alaire
+Auristela
+Bastian
+Breena
+Brielle
+Briallan
+Briseis
+Cambria
+Cara
+Carys
+Caspian
+Cassia
+Cassiel
+Cassiopeia
+Cassius
+Chaniel
+Cora
+Corbin
+Cyprian
+Daire
+Darius
+Destin
+Drake
+Drystan
+Dagen
+Devlin
+Devlyn
+Eira
+Eirian
+Elysia
+Eoin
+Evadne
+Eliron
+Evanth
+Fineas
+Finian
+Fyodor
+Gareth
+Gavriel
+Griffin
+Guinevere
+Gaerwn
+Ginerva
+Hadriel
+Hannelore
+Hermione
+Hesperos
+Iagan
+Ianthe
+Ignacia
+Ignatius
+Iseult
+Isolde
+Jessalyn
+Kara
+Kerensa
+Korbin
+Kyler
+Kyra
+Katriel
+Kyrielle
+Leala
+Leila
+Lilith
+Liora
+Lucien
+Lyra
+Leira
+Liriene
+Liron
+Maia
+Marius
+Mathieu
+Mireille
+Mireya
+Maylea
+Meira
+Natania
+Nerys
+Nuriel
+Nyssa
+Neirin
+Nyfain
+Oisin
+Oralie
+Orion
+Orpheus
+Ozara
+Oleisa
+Orinthea
+Peregrine
+Persephone
+Perseus
+Petronela
+Phelan
+Pryderi
+Pyralia
+Pyralis
+Qadira
+Quintessa
+Quinevere
+Raisa
+Remus
+Rhyan
+Rhydderch
+Riona
+Renfrew
+Saoirse
+Sarai
+Sebastian
+Seraphim
+Seraphina
+Sirius
+Sorcha
+Saira
+Sarielle
+Serian
+Séverin
+Tavish
+Tearlach
+Terra
+Thalia
+Thaniel
+Theia
+Torian
+Torin
+Tressa
+Tristana
+Uriela
+Urien
+Ulyssia
+Vanora
+Vespera
+Vasilis
+Xanthus
+Xara
+Xylia
+Yadira
+Yseult
+Yakira
+Yeira
+Yeriel
+Yestin
+Zaira
+Zephyr
+Zora
+Zorion
+Zaniel
+Zarek`
+
+const monikerString = `Orlan
+Shalid
+Roosu
+Glass
+Templin
+Bikker
+Lidyan
+Colver
+Driver
+Rider
+The Weary
+Of Gelfdin
+The moist
+Dartimon
+The Merciless
+The Fierce
+The ugly
+The bulbous
+The Perpetually wet
+The Bold
+Of lasling
+Rudger
+The onry
+The grave
+The furious
+Tootles
+Bumkiss
+Flours
+Stega
+Gantry
+Rhodega`
+
+const raceString = `Human
+Wood Elf
+Feral Elf
+Temple Elf
+Mountain Dwarf
+Sea Dwarf
+Swamp Dwarf
+Sun Orc
+Moon Orc
+Meeker
+Groog
+Dragonkin
+Scyric`
+
+const professionString = `Soldier
+Thief
+Urchin
+Noble
+Scholar
+Priest
+Performer
+Artisan
+Farmer
+Sailor
+Acolyte
+Charlatan
+City Watch
+Investigator
+Craftsmen
+Courtier
+Entertainer
+Gladiator
+Faction Agent
+Far Traveler
+Folk Hero
+Gambler
+Hermit
+Inheritor
+Mercenary
+Knight
+Outlander
+Sage
+Pirate
+Smuggler
+Bounty Hunter
+Tax Collector?
+Fisherman?
+Merchant
+Town Drunk
+Village Idiot
+Herald
+Historian
+Librarian
+Bard
+Politician
+Hairdresser
+Lawyer
+Cook
+Entrepreneur
+Businessman
+Veterinarian
+Shopkeep
+Servant
+Apprentice
+School teacher
+Accountant
+Athlete
+Inventor
+Gatherer
+Scout
+Collector
+Student
+Town Crier
+Executioner
+Butcher
+Baker
+Candlestick Maker
+Medic/Doctor
+For small races:
+pack leader
+breeder
+Merchant
+Noble
+Academic
+Religious
+Craftsman
+Worker/Laborer
+Ruffian
+Rural
+Soldier
+Sailor
+Professional
+Entertainer
+Government`
+
+const valueString = `Abundance
+Acceptance
+Accountability
+Achievement
+Advancement
+Adventure
+Advocacy
+Ambition
+Appreciation
+Attractiveness
+Autonomy
+Balance
+Being the Best
+Benevolence
+Boldness
+Brilliance
+Calmness
+Caring
+Challenge
+Charity
+Cheerfulness
+Cleverness
+Community
+Commitment
+Compassion
+Cooperation
+Collaboration
+Consistency
+Contribution
+Creativity
+Credibility
+Curiosity
+Daring
+Decisiveness
+Dedication
+Dependability
+Diversity
+Empathy
+Encouragement
+Enthusiasm
+Ethics
+Excellence
+Expressiveness
+Fairness
+Family
+Friendships
+Flexibility
+Freedom
+Fun
+Generosity
+Grace
+Growth
+Flexibility
+Happiness
+Health
+Honesty
+Humility
+Humor
+Inclusiveness
+Independence
+Individuality
+Innovation
+Inspiration
+Intelligence
+Intuition
+Joy
+Kindness
+Knowledge
+Leadership
+Learning
+Love
+Loyalty
+Making a Difference
+Mindfulness
+Motivation
+Optimism
+Open-Mindedness
+Originality
+Passion
+Performance
+Personal Development
+Proactive
+Professionalism
+Quality
+Recognition
+Risk Taking
+Safety
+Security
+Service
+Spirituality
+Stability
+Peace
+Perfection
+Playfulness
+Popularity
+Power
+Preparedness
+Proactivity
+Professionalism
+Punctuality
+Recognition
+Relationships
+Reliability
+Resilience
+Resourcefulness
+Responsibility
+Responsiveness
+Security
+Self-Control
+Selflessness
+Self Preservation
+Simplicity
+Stability
+Success
+Teamwork
+Thankfulness
+Thoughtfulness
+Traditionalism
+Trustworthiness
+Understanding
+Uniqueness
+Usefulness
+Versatility
+Vision
+Warmth
+Wealth
+Well-Being
+Wisdom
+Zeal`
+
+const goodDispString = `Interested
+Romantic
+Intimidated
+Eager
+Earnest
+Loyal
+Charmed
+Frightened
+Entertained
+Indebted
+Trusting
+Friendly
+Flattered
+Impressed
+Mentor
+Cool
+Empassioned
+Chummy
+Captivated
+Guilty`
+
+const badDispString = `Bored
+Hostile
+Disinterested
+Competitive
+Impatient
+Defensive
+Repulsed
+Suspicious
+Annoyed
+Afraid
+Onry
+Confused
+Cranky
+Ashamed
+Desperate`
+
+const desireString = `Water
+Food
+Clothing
+Shelter
+Protection against assault or injury
+Adequate money
+Steady employment
+Good health
+Protection of private property
+Friendship
+Romance
+Intimacy
+Family
+Independence
+Compensation
+Respect
+Promotion
+Credit
+Gratitude
+Appreciation
+Higher education
+Spiritual enlightenment
+Artistic pursuits
+Travel and experience
+Altruistic and charitable contributions to others
+I would die to recover an ancient artifact of my faith that was lost long ago.
+I will someday get revenge on the corrupt temple hierarchy who branded me a heretic.
+I owe me life to the priest who took me in when my parents died.
+Everything I do is for the common people.
+I will do anything to protect the temple where I served.
+I seek to preserve a sacred text that my enemies consider heretical and seek to destroy.
+I fleeced the wrong person and must work to ensure that this individual never crosses paths with me or those I care about.
+I owe everything to my mentor--a horrible person who's probably rotting in jail somewhere.
+Somewhere out there I have a child who doesn't know me. I'm making the world better for him or her.
+I come from a noble family, and one day I'll reclaim my lands and title from those who stole them from me.
+A powerful person killed someone I love. Someday soon, I'll have my revenge.
+I swindled and ruined a person who didn't deserve it. I seek to atone for my misdeeds but might never be able to forgive myself.
+I'm trying to pay off an old debt I owe to a generous benefactor.
+My ill-gotten gains go to support my family.
+Something important was taken from me, and I aim to steal it back.
+I will become the greatest thief that ever lived.
+I'm guilty of a terrible crime. I hope I can redeem myself for it.
+Someone I loved died because of a mistake I made. That will never happen again.
+My instrument is my most treasured possession, and it reminds me of someone I love.
+Someone stole my precious instrument, and someday I'll get it back.
+I want to be famous, whatever it takes.
+I idolize a hero of the old tales and measure my deeds against that person's.
+I will do anything to prove myself superior to me hated rival.
+I would do anything for the other members of my old troupe.
+I have a family, but I have no idea where they are. One day, I hope to see them again.
+I worked the land, I love the land, and I will protect the land.
+A proud noble once gave me a horrible beating, and I will take my revenge on any bully I encounter.
+My tools are symbols of my past life, and I carry them so that I will never forget my roots.
+I protect those who cannot protect themselves.
+I wish my childhood sweetheart had come with me to pursue my destiny.
+The workshop where I learned my trade is the most important place in the world to me.
+I created a great work for someone, and then found them unworthy to receive it. I'm still looking for someone worthy.
+I owe my guild a great debt for forging me into the person I am today.
+I pursue wealth to secure someone's love.
+One day I will return to my guild and prove that I am the greatest artisan of them all.
+I will get revenge on the evil forces that destroyed my place of business and ruined my livelihood.
+Nothing is more important than the other members of my hermitage, order, or association.
+I entered seclusion to hide from the ones who might still be hunting me. I must someday confront them.
+I'm still seeking the enlightenment I pursued in my seclusion, and it still eludes me.
+I entered seclusion because I loved someone I could not have.
+Should my discovery come to light, it could bring ruin to the world.
+My isolation gave me great insight into a great evil that only I can destroy.
+I will face any challenge to win the approval of my family.
+My house's alliance with another noble family must be sustained at all costs.
+Nothing is more important that the other members of my family.
+I am in love with the heir of a family that my family despises.
+My loyalty to my sovereign is unwavering.
+The common folk must see me as a hero of the people.
+My family, clan, or tribe is the most important thing in my life, even when they are far from me.
+An injury to the unspoiled wilderness of my home is an injury to me.
+I will bring terrible wrath down on the evildoers who destroyed my homeland.
+I am the last of my tribe, and it is up to me to ensure their names enter legend.
+I suffer awful visions of a coming disaster and will do anything to prevent it.
+It is my duty to provide children to sustain my tribe.
+It is my duty to protect my students.
+I have an ancient text that holds terrible secrets that must not fall into the wrong hands.
+I work to preserve a library, university, scriptorium, or monastery.
+My life's work is a series of tomes related to a specific field of lore.
+I've been searching my whole life for the answer to a certain question.
+I sold my soul for knowledge. I hope to do great deeds and win it back.
+I'm loyal to my captain first, everything else second.
+The ship is most important--crewmates and captains come and go.
+I'll always remember my first ship.
+In a harbor town, I have a paramour whose eyes nearly stole me from the sea.
+I was cheated of my fair share of the profits, and I want to get my due.
+Ruthless pirates murdered my captain and crewmates, plundered our ship, and left me to die. Vengeance will be mine.
+I would lay down my life for the people I served with.
+Someone saved my life on the battlefield. To this day, I will never leave a friend behind.
+My honor is my life.
+I'll never forget the crushing defeat my company suffered or the enemies who dealt it.
+Those who fight beside me are those worth dying for.
+I fight for those who cannot fight for themselves.
+My town or city is my home, and I'll fight to defend it.
+I sponsor an orphanage to keep others from enduring what I was forced to endure.
+I owe my survival to another urchin who taught me to live on the streets.
+I owe a debt I can never repay to the person who took pity on me.
+I escaped my life of poverty by robbing an important person, and I'm wanted for it.
+No one else is going to have to endure the hardships I've been through`
+
+const ageString = `Elderly
+Youth
+Middle Aged
+Young Adult
+Seasoned`
+
+const statusString = `Poor
+Wealthy
+Middle Class
+Destitute
+Bougie`;
+
+const tagString = `Underling
+Authority
+Elites
+Noble
+Local
+Foreigner
+Outcast
+Criminal
+Plotter
+Professional
+Craftsman
+Soldier
+Hermit
+Guilty
+Uneducated
+Outlander
+Outlaw
+Peasant
+Thief
+Bard
+Bounty Hunter
+Expolorer
+Journeyman
+Sailor
+Charlatan
+Priest
+Merchant`
+
+const options = {
+    names : nameString.split('\n'),
+    monikers : monikerString.split('\n'),
+    races : raceString.split('\n'),
+    profesions : professionString.split('\n'),
+    values : valueString.split('\n'),
+    goodDispositions : goodDispString.split('\n'),
+    badDispositions : badDispString.split('\n'),
+    desires : desireString.split('\n'),
+    ages : ageString.split('\n'),
+    statuses : statusString.split('\n'),
+    tags : tagString.split('\n')
+}
+
+function generateNPC(options){
+    let rand = (total) => Math.floor(Math.random() * total);
+    let rand6 = () => Math.floor(Math.random() *5) +1;
+
+    let nameMaker = (first,last) => `${first} ${last}`;
+    document.getElementById("name").innerHTML = nameMaker(options.names[rand(options.names.length)],options.monikers[rand(options.monikers.length)]);
+
+    document.getElementById("race").innerHTML = options.races[rand(options.races.length)];
+    document.getElementById("profession").innerHTML = options.profesions[rand(options.profesions.length)];
+
+    //Values
+    document.getElementById("value1").innerHTML = options.values[rand(options.values.length)];
+    document.getElementById("goodValue1").innerHTML = options.goodDispositions[rand(options.goodDispositions.length)];
+    document.getElementById("badValue1").innerHTML = options.badDispositions[rand(options.badDispositions.length)];
+
+    document.getElementById("value2").innerHTML = options.values[rand(options.values.length)];
+    document.getElementById("goodValue2").innerHTML = options.goodDispositions[rand(options.goodDispositions.length)];
+    document.getElementById("badValue2").innerHTML = options.badDispositions[rand(options.badDispositions.length)];
+
+    document.getElementById("value3").innerHTML = options.values[rand(options.values.length)];
+    document.getElementById("goodValue3").innerHTML = options.goodDispositions[rand(options.goodDispositions.length)];
+    document.getElementById("badValue3").innerHTML = options.badDispositions[rand(options.badDispositions.length)];
+
+    //Charisma 
+
+    document.getElementById("aggressive").innerHTML = rand6() + rand6() + rand6() + rand6();
+    document.getElementById("goodAggressive").innerHTML = options.goodDispositions[rand(options.goodDispositions.length)];
+    document.getElementById("badAggressive").innerHTML = options.badDispositions[rand(options.badDispositions.length)];
+
+    document.getElementById("suave").innerHTML = rand6() + rand6() + rand6() + rand6();
+    document.getElementById("goodSuave").innerHTML = options.goodDispositions[rand(options.goodDispositions.length)];
+    document.getElementById("badSuave").innerHTML = options.badDispositions[rand(options.badDispositions.length)];
+
+    document.getElementById("diplomatic").innerHTML = rand6() + rand6() + rand6() + rand6();
+    document.getElementById("goodDiplomatic").innerHTML = options.goodDispositions[rand(options.goodDispositions.length)];
+    document.getElementById("badDiplomatic").innerHTML = options.badDispositions[rand(options.badDispositions.length)];
+
+    document.getElementById("sincere").innerHTML = rand6() + rand6() + rand6() + rand6();
+    document.getElementById("goodSincere").innerHTML = options.goodDispositions[rand(options.goodDispositions.length)];
+    document.getElementById("badSincere").innerHTML = options.badDispositions[rand(options.badDispositions.length)];
+
+    function tagMaker(){
+        let tags = []
+        tags.push(options.ages[rand(options.ages.length)])
+        tags.push(options.statuses[rand(options.statuses.length)])
+        for (let i = 0;i<3;i++){
+            tags.push(options.tags[rand(options.tags.length)])
+        }
+        return tags
+    }
+
+    document.getElementById("tags").innerHTML = tagMaker();
+
+    document.getElementById("desires").innerHTML = options.desires[rand(options.desires.length)];
+
+
+}
+
+
+
