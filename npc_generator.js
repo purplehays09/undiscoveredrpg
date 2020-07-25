376c6c1e883a3c262deae0bbe9a103344cad4e50
purplehays09 <purplehays527@gmail.com>
2020-07-24 19:02:43 -0600

    added an encounter generator

--- npc_generator.js
+++ npc_generator.js
@@ -635,6 +635,336 @@ Charlatan
 Priest
 Merchant`
 
+const rpSetUpString = `Merchant
+Giant
+Alchemist
+Blacksmith
+Guard
+Librarian
+Student
+Guild Member
+Gang Leader
+Gang
+King
+Traveler
+Hermit
+Animal
+Performer
+Thief
+Soldier
+Spy
+Folk Hero
+Pirate
+Sailor
+Urchin
+Homeless
+Priest
+Skeever
+Maid
+Lonely old man
+Super hot hag (technically ugly hag)
+Kitty cat druid
+8 year old girl
+Orphans
+underage teens
+teenage mutant Ninja purrtles
+young skeever child
+gnome
+ogre
+very short human, human version of dwarf
+stripper dwarf
+the law
+the mayor
+garbage man 
+plumber
+salesman
+gaggle of hags
+boy band
+private investigator
+a doctor
+sniffing dog finder dog
+newly weds
+street wise youths
+baker
+candlestick maker
+butcher
+spy
+middle school girls
+prisoner
+ruffian
+actor
+glee club
+pre teens`
+
+const rpHookString = `Connected to a backstory
+Connected to the story
+Connected to an instinct
+Is hungry
+Is bleeding out
+Running away from (roll again)
+Owes Money to (roll again)
+Is missing a family member
+Is hiding out
+is out of breath
+is suspicious of you
+Needs you to help him
+Talks in rhyme
+m
+narrates their life
+is mixing chemicals
+is cooking
+is cleaning something
+Has a bomb on them
+Needs to cook for you
+needs 50 gold to survive the night
+needs help collecting a debt
+is in disguise
+finds you irresistibly attractive
+The floor is lava
+lost a balloon
+needs food
+Need a home
+know it all
+urgently needs to pee
+needlessly bossy
+always wants a sandwhich 
+thinks you are a wizard 
+believes that you are the mayor
+hates books
+doesn't believe in skeevers
+paranoid of everything
+speaks in rhyme
+sings everything they speak
+has no filter
+naggy 
+has very loud disruptive farts 
+wants to sell you watches
+doesn't like the way you look
+dementia 
+wants a baby 
+needs to be spoonfed
+lost his shadow 
+punching the wall
+wants a pair of eyes 
+needs help getting their book published 
+playing wonderwall
+talking to rats in the ally, rats are enamored. Do they understand him?
+wants you to buy them alcholol 
+shoves you and declares that they are homeless and new in town 
+wants to know why?!
+you owe them
+doesn't understand the concept of possesion
+making soup
+licking their lips alot
+piss drunk, wants your (whatever new thing you just got)
+druid got stuck mid transition 
+talks too loud
+speaks only in stage whispers
+thinks he is a skeever/body dsymorphia
+believes you are their dead father
+believes you are the guy that owes him money
+thinks you have pretty eyes, wants your eyes for himself
+passed out drunk with all of his loot easy for the taking
+can't poop
+has no arms or legs
+wants your help baking a birthday cake
+wants you to slap an ogre that was rude, he can't cause he can't reach
+check if there is something in their eye
+does my breath smell?
+find me truffles with my pig?
+neighbor stole my bushes
+someone stole my grandmothers recipe
+wants an orange kitten, it has to be orange
+need one last item for their potion
+rub their feet, bunions really hurt something awful
+brush their hair
+help win over their crush
+the love of their life is getting married today
+someone burned down their home
+rival pub is close to recreating their secret recipe 
+need help digging a hole
+fell down a drain/manhole
+wants you to cut off their arm
+doesn't remember who they are
+thinks that you are their childhood best friend
+believes that they owe you a life debt for saving them
+desperately wants a master
+wants you to get rid of someone for them
+peeing on a pile of books
+making cheese 
+hates gingers
+hates what race they are/doesn't realize they are that race
+you haven't seen them blink for much too long. Do they blink?
+peed themself
+splashing in puddles
+my yard!!
+thinks you stink, when they stink.
+petting imaginary animals
+talking to themself
+singing a beautiful song with emotion/realize its a really bad thing they are singing
+this is my town
+whats the password?
+paid to kill you, better offer?
+needs you to pretend to be their lover
+gossiping about you
+krumping
+running for office
+needs a boost over a wall
+kiss them to distract the people looking for them 
+wants your opinion
+trying to get access into exclusive place
+sneaking into a window
+pretend to be their dad
+did you poop here?! 
+is this yours? 
+thinks you are the funniest person they've met
+running an illegal puppy mill
+can you taste this?
+do you smell that? (it's them)
+selling counterfiet pants, pretty good quality
+hard of hearing
+wants you to teach them how to read so that they might better their life
+knitting a blanket
+find them a wife
+need someone to marry them to lover tomorrow
+needs help moving their couch
+interested in their homemade handmade jewelry? It's very ugly
+making mud pies
+wrapped in lots of blankets rocking back and forth
+brushing their teeth
+breathing really loud
+panic attack
+raccon infestation help
+drunk, needs help sobering up to impress important people
+compliments you too much 
+walking a dog
+shivering like a lot
+stepped in dog poops
+pitching a tent
+watching you 
+tending a garden
+choaking on something
+hanging from a building barely holding on
+stealing bread
+picking their nose with a finger way too deep
+wearing the exact same outfit as you. Every detail the same.
+you look just like her. And walks away.
+same exact voice as one of the players
+mirrors all of your movements
+tries to mimick your mannerisms/behavior as you talk to them. Studying you.`
+
+const rpTrademarkString = `poop brown eyes
+Has an Eyepatch
+Cleft lip
+purple hair
+Skin glows
+different colored eyes
+Black horned rimmed glasses
+Scruffy beard
+Chin of all chins
+Nose of all noses
+Belches consstantly
+Massive moustache
+no hair
+soaking in sweat
+Bright red hair
+blind
+wart
+sleevless shirt
+is overweight
+Wears only gold
+Emerald Broach
+Long finger nails
+no eyebrows
+whiskers for eyebrows
+too many birthmarks
+glowing freckles
+has no teeth 
+yellow teeth
+too white of teeth
+celebrity attractive
+moldy hair
+blind parrot on their shoulder
+a mouse on their head
+6 fingered hand
+very long fingernails
+very small wrists 
+wears too much leather
+wearing a tin hat
+crooked smile
+very large too large lips (how do they speak size)
+patchy beard 
+missing all their fingers/ paddle hands
+wearing way too much colonge/perfume
+looks nice and clean but smells of really strong BO
+really big hoop earrings
+black eye, literally one eye is black
+has a black eye
+leathery skin
+teeth are too big for their face
+crazy eyes
+hair past the floor with odds and bits in it
+cat eyes/literally
+you can't see if they actually have lips or if it is a line that is a mouth
+way too hairy of arms
+white blonde mustache
+no eyebrows
+bleeding lip
+missing an ear and a half
+no fingernails
+bruise around their neck
+dry patchy skin
+no hair anywhere
+covered in glitter
+little daggers attached to each finger/like nails
+clearly fake eyebrows
+gold tooth
+one large boob
+pot belly
+too short of bangs
+bangs
+yellow eyeshadow
+flowers in their hair
+dagger behind their ear like a pencil
+wearing a bow in their hair
+shifty eyes
+nude
+face tattoo
+sticky out ears
+kind eyes
+eyebrow hairs are too long for how thick the eyebrow is
+has a large spider on their shoulder
+wearing all yellow
+looks generations older than they are
+looks way too young for their age
+has an endearing lazy eye
+very small nose/ too small
+very curly hair
+ginger
+more frekcles than non freckles. Maybe the light parts are the freckles
+wedding ring
+ring on a necklace
+too many watches
+a ring on every finger
+scantily clad
+unacceptable amount of cleavage
+buttcrack showing
+sweaty pits
+smells like a butt farted
+beautiful smile
+doesn't have enough teeth
+isn't wearing pants
+a nose that adds character 
+10 inches above average tall
+large tummy with no bum
+mute
+drooly
+quite twitchy
+embarassing toots. You have to pretend not to hear them or you will offend them.
+wearing just a pillowcase
+has whiskers
+dirty body in nice clean expensive tuxedo`
+
 const options = {
     names : nameString.split('\n'),
     monikers : monikerString.split('\n'),
@@ -647,6 +977,12 @@ const options = {
     ages : ageString.split('\n'),
     statuses : statusString.split('\n'),
     tags : tagString.split('\n')
+}
+
+const encounterOptions = {
+    setUp : rpSetUpString.split('\n'),
+    hook : rpHookString.split('\n'),
+    trademark : rpTrademarkString.split('\n')
 }
 
 function generateNPC(options){
@@ -708,4 +1044,15 @@ function generateNPC(options){
 }
 
 
+function generateEncounter(options){
 
+    let rand = (total) => Math.floor(Math.random() * total);
+
+    document.getElementById("set-up").innerHTML = options.setUp[rand(options.setUp.length)];
+
+    document.getElementById("hook").innerHTML = options.hook[rand(options.hook.length)];
+
+    document.getElementById("trademark").innerHTML = options.trademark[rand(options.trademark.length)];
+
+
+}
