a14a3d2eb750c427ac3e42345da017deb99fece6
purplehays09 <purplehays527@gmail.com>
2020-08-15 11:32:16 -0600

    added the beginning of the map.js components

---
+++ map_data.js
@@ -0,0 +1,27 @@
+const data =   [
+    {
+        x:1,
+        y:1,
+        z:0,
+        Positions:[],
+        Materials:[],
+        Items:[],
+        Characters:[]
+    }    
+]
+
+const mapObj = {
+    xRange = 8,
+    yrange = 8,
+    
+
+}
+
+
+function createMapObj (mapObj){
+    for(let i = 0;i<8;i++){
+        data.
+    }
+
+}
+
